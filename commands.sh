89eb63d01ff127f4447486950edf63fa4e97a1b0
Esmailton Gomes <contato.esmailtongomes@gmail.com>
2024-03-15 07:41:26 -0300

    test:  include  test
     - Adding test execution in script

--- commands.sh
+++ commands.sh
@@ -5,4 +5,7 @@ python manage.py collectstatic --noinput
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
+echo "Running Test..."
+python manage.py test 
+
 gunicorn --workers=1 --threads=2 setup.wsgi:application --bind 0.0.0.0:8000
